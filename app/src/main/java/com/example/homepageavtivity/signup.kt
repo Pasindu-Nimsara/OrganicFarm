6d4a6df95d837270f9c3559a85d4c67391211b83
AORUS <it21208980@my.sliit.lk>
2023-05-07 12:38:26 +0530

    home Page plus login registration Create user

--- app/src/main/java/com/example/homepageavtivity/signup.kt
+++ app/src/main/java/com/example/homepageavtivity/signup.kt
@@ -4,16 +4,74 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class signup : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
-        val loginBtn2 = findViewById<Button>(R.id.signupbtn2)
-        loginBtn2.setOnClickListener {
-            val Intent = Intent(this, login::class.java)
-            startActivity(Intent)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+
+
+        val signupBtn: Button = findViewById(R.id.signupbtn2)
+        signupBtn.setOnClickListener{
+            performSignup()
         }
+
+    }
+
+    private fun performSignup() {
+
+        val email = findViewById<EditText>(R.id.editTextEmail)
+        val password = findViewById<EditText>(R.id.editTextPassword)
+
+        if(email.text.isEmpty()|| password.text.isEmpty()){
+            Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+
+        val inputEmail = email.text.toString().trim()
+        val inputPassword = password.text.toString().trim()
+
+
+        auth.createUserWithEmailAndPassword(inputEmail, inputPassword)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success
+                    val intent = Intent(this, WlcomeActivity::class.java)
+                    startActivity(intent)
+
+                    Toast.makeText(
+                        baseContext,
+                        "SignUp Success.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+
+                } else {
+                    // If sign in fails, display a message to the user.
+
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+
+                }
+            }
+            .addOnFailureListener{
+                Toast.makeText(this, "Error Occurred ${it.localizedMessage}", Toast.LENGTH_SHORT)
+            }
+
     }
 }
