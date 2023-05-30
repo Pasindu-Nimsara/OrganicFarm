c7407d3fa232b65228f113ed9fdfd59079f2149d
Wijesinghe K.S.K <101441374+kithminiwijesinghe@users.noreply.github.com>
2023-05-30 17:29:13 +0530

    Add files via upload

--- app/src/main/java/com/example/loginregistration/MainActivity.kt
+++ app/src/main/java/com/example/loginregistration/MainActivity.kt
@@ -1,27 +1,28 @@
-package com.example.loginregistration
+package com.example.myapplication
 
 import android.content.Intent
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var database : DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.startBtn.setOnClickListener(View.OnClickListener{
+            val intent = Intent(this@MainActivity, AdminDashboard::class.java)
+            startActivity(intent)
+        })
 
-        val loginBtn = findViewById<Button>(R.id.loginbtn)
-        loginBtn.setOnClickListener{
-            val Intent = Intent(this, login::class.java)
-            startActivity(Intent)
-        }
-
-
-        val signupBtn = findViewById<Button>(R.id.signupbtn)
-        signupBtn.setOnClickListener{
-            val Intent = Intent(this, signup::class.java)
-            startActivity(Intent)
-        }
     }
 }
